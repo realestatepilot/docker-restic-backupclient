f41dee296813de6c59aec654747525b6b081615d
Michael Wyraz <michael@wyraz.de>
2019-02-12 13:37:01 +0100

    use --host (was --hostname wich is deprecated)

--- backup_client.py
+++ backup_client.py
@@ -106,7 +106,7 @@ def run_backup():
 		subprocess.check_call([
 			'restic',
 			'backup',
-			'--hostname',get_env('BACKUP_HOSTNAME'),
+			'--host',get_env('BACKUP_HOSTNAME'),
 			backup_root
 			],stderr=subprocess.STDOUT)
 		log.info('Backup finished.')
