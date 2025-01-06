652a41255c43a7a93fb564c44a2b644b5fb761de
Justus Stahlhut <justus.stahlhut@realestatepilot.com>
2025-01-06 14:55:04 +0100

    add pg-17 repo

--- test/test_postgres.sh
+++ test/test_postgres.sh
@@ -2,6 +2,9 @@
 
 # dependencies
 sudo apt update
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+sudo apt update
 sudo apt install -y postgresql-17 restic
 
 # start postgres
