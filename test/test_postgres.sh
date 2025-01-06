260e11fbe4f8d46625e3d629c8620e8674951d6b
Justus Stahlhut <justus.stahlhut@realestatepilot.com>
2025-01-06 14:59:32 +0100

    remove -U root

--- test/test_postgres.sh
+++ test/test_postgres.sh
@@ -16,7 +16,7 @@ sudo -u postgres psql -c "CREATE USER root WITH PASSWORD 'guest';"
 # ingest data
 sudo -u postgres createdb testdb 
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE testdb TO root;"
-sudo -u postgres psql -U root -d testdb -f test/data/artists.sql
+sudo -u postgres psql -d testdb -f test/data/artists.sql
 
 # number of expected entries in restored table
 EXPECTED=$(sudo -u postgres psql -d testdb -c "SELECT COUNT(*) FROM artist;" -t -A)
