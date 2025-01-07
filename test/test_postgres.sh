66b5726085a2e9e73b9c620f902b26fe1c191f2b
Justus Stahlhut <justus.stahlhut@realestatepilot.com>
2025-01-07 08:17:13 +0100

    grant privileges after data ingestion

--- test/test_postgres.sh
+++ test/test_postgres.sh
@@ -15,8 +15,8 @@ sudo -u postgres psql -c "CREATE USER root WITH PASSWORD 'guest';"
 
 # ingest data
 sudo -u postgres createdb testdb 
+sudo -u postgres psql -d testdb -f test/data/artists-postgres.sql
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE testdb TO root;"
-sudo -u postgres psql -U root -d testdb -f test/data/artists-postgres.sql
 
 # number of expected entries in restored table
 EXPECTED=$(sudo -u postgres psql -d testdb -c "SELECT COUNT(*) FROM artist;" -t -A)
